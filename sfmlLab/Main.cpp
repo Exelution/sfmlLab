2e720a7cf425eb2e15a7afe6bcf7c327fc2f4c08
Exelution <74369712+Exelution@users.noreply.github.com>
2021-11-03 22:30:35 +0200

    structuring code

--- sfmlLab/Main.cpp
+++ sfmlLab/Main.cpp
@@ -1,246 +1,233 @@
 
+
 #include "Main.h"
 
-class LineChart : VertexArray
+void gameRunning(Game& GameInstance)
 {
-public:
-    LineChart(int pPoints, pair<int, int> pLocation, pair<int, int> pSize, Color pColor = Color::Red);
-
-    void DrawSeries(vector<float>& pValsX, vector<float>& pValsY, RenderWindow& win, LineChart& instance);
-
-    void DrawBackground(Color color, float Offset, RenderWindow& win);
-
-private:
-    int Points = 0;
-    pair<int, int> Location;
-    pair<int, int> Size;
-    Color color = Color::Red;
-};
+    if (GameInstance.run())
+    {
+        gameRunning(GameInstance);
+    }
+}
 
 int main()
 {
-
-    Model model;
-    Math math;
-    ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    int scale = 300;
-    float windowXSize = model.CirclesXCount * model.CircleRadius * 2 + model.DrawOffset * 2;
-    float windowYSize = model.CirclesYCount * model.CircleRadius * 2 + model.DrawOffset * 2;
-    model.CalculateSize();
-    LineChart Chart1(model.CirclesXCount, model.GraficLoc, model.GraficSize);
-    LineChart Chart2(model.CirclesXCount, model.GraficLoc, model.GraficSize, model.clSecondAtom);
-
-    RenderWindow window(VideoMode(windowXSize + 200, windowYSize + 400), "SFML Lab", Style::Default, settings);
-
-    // window.setFramerateLimit(600);
-    model.SetCircleColor();
-
-    sf::Event event;
-    Clock clock;
-    float time = 0.0f;
-    float DeltaTime = 0.0f;
-    float LastTime = 0.0f;
-    float FPSTimer = 0.0f;
-
-    model.DrawCircleGrid(window);
-
-    while (window.isOpen())
-    {
-        time = clock.getElapsedTime().asSeconds();
-        DeltaTime = time - LastTime;
-        LastTime = time;
-
-        FPSTimer += DeltaTime;
-
-        cout << "timer: " << time << endl;
-        cout << "time: " << DeltaTime << endl;
-        cout << "truefps: " << 1 / DeltaTime << endl;
-        cout << "fps: " << 1 / FPSTimer << endl;
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed) window.close();
-        }
-
-        if (FPSTimer >= 0.025f)
-        {
-           // window.clear();
-
-            model.DrawCircles(window, time);
-            model.CalculateAtoms();
-            Chart1.DrawBackground(Color(20, 20, 20), 20, window);
-            Chart1.DrawSeries(model.ValsX, model.ValsY1, window, Chart1);
-            Chart2.DrawSeries(model.ValsX, model.ValsY2, window, Chart2);
-
-            FPSTimer = 0.0f;
-        }
-
-        if (Keyboard::isKeyPressed(Keyboard::C))
-        {
-
-             model.ClearGrid(window);
-        }
-
-        if (Keyboard::isKeyPressed(Keyboard::Escape))
-        {
-            window.close();
-        }
-        window.display();
-    }
-
+    Game game;
+    gameRunning(game);
     return 0;
 }
 
-void Model::SetCircleColor()
+Game::Game()
+    : mWindow(sf::VideoMode(540, 840), "SFML Application"),                                                                    //
+                                                                                                                               //
+      Atom1Chart(atomGrid.GetAtomOnSide(),                                                                                     //
+          sf::Vector2f(atomGrid.Location.x + atomGrid.SideSize / atomGrid.GetAtomOnSide() / 2,                                 //
+              atomGrid.Location.y + atomGrid.SideSize * 3 / 2 + 20.f),                                                         //
+          sf::Vector2f(atomGrid.SideSize * (atomGrid.GetAtomOnSide() - 1) / atomGrid.GetAtomOnSide(), atomGrid.SideSize / 2),  //
+          atomGrid.clFirstAtom),                                                                                               //
+                                                                                                                               //
+      Atom2Chart(atomGrid.GetAtomOnSide(),                                                                                     //
+          sf::Vector2f(atomGrid.Location.x + atomGrid.SideSize / atomGrid.GetAtomOnSide() / 2,                                 //
+              atomGrid.Location.y + atomGrid.SideSize * 3 / 2 + 20.f),                                                         //
+          sf::Vector2f(atomGrid.SideSize * (atomGrid.GetAtomOnSide() - 1) / atomGrid.GetAtomOnSide(), atomGrid.SideSize / 2),  //
+          atomGrid.clSecondAtom)                                                                                               //
 {
-    FirstAtom.setFillColor(clFirstAtom);
-    FirstAtom.setRadius(CircleRadius);
-
-    SecondAtom.setFillColor(clSecondAtom);
-    SecondAtom.setRadius(CircleRadius);
-
-    Vacancy.setFillColor(clVacancy);
-    Vacancy.setRadius(CircleRadius);
-
-    return;
 }
-void Model::DrawCircles(RenderWindow& win, float time)
+
+bool Game::run()
 {
-    Math math;
-    for (int i = 0; i < CirclesYCount; i++)
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
+    BeginPlay();
+
+    while (mWindow.isOpen())
     {
-        int x = VacancyArray[i].first;
-        int y = VacancyArray[i].second;
-
-        int right = 0;
-        int up = 0;
-
-        if (math.RandBool())
-            right = 1;
-        else
-            right = -1;
-
-        if (math.RandBool())
-            up = 1;
-        else
-            up = -1;
-        if (!CirclesArray[x + right][y + up]) return;
-        if (x + right < 0 || x + right >= CirclesXCount - 1) return;
-        if (y + up < 0 || y + up >= CirclesYCount - 1) return;
-
-        if (CirclesArray[x + right][y + up] == 3)
-            return;
-        else
+        processEvents();
+        timeSinceLastUpdate += clock.restart();
+        while (timeSinceLastUpdate > TimePerFrame)
         {
-            CirclesArray[x][y] = CirclesArray[x + right][y + up];
-            DrawCircleColor(win, x, y, CirclesArray[x + right][y + up]);
-            CirclesArray[x + right][y + up] = 3;
-            DrawCircleColor(win, x + right, y + up, 3);
-            VacancyArray[i].first = x + right;
-            VacancyArray[i].second = y + up;
+            timeSinceLastUpdate -= TimePerFrame;
+            processEvents();
+            update(TimePerFrame);
+        }
+        render(TimePerFrame);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
+        {
+            return true;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            return false;
         }
     }
 }
 
-void Model::DrawCircleGrid(RenderWindow& win)
+void Game::processEvents()
 {
-    Math math;
-    if (bGridDrawed) return;
-
-    float localX = 2 * CircleRadius;
-    float localY = 2 * CircleRadius;
-
-    for (int j = 0; j < CirclesYCount; j++)
-        for (int i = 0; i < CirclesXCount; i++)
+    sf::Event event;
+    while (mWindow.pollEvent(event))
+    {
+        switch (event.type)
         {
+            case sf::Event::KeyPressed: handlePlayerInput(event.key.code, true); break;
+            case sf::Event::KeyReleased: handlePlayerInput(event.key.code, false); break;
+            case sf::Event::Closed: mWindow.close(); break;
+        }
+    }
+}
 
-            if (i < CirclesXCount / 2)
-            {
-                FirstAtom.setPosition(localX * i + DrawOffset, localY * j + DrawOffset);
-                win.draw(FirstAtom);
-                win.display();
-                CirclesArray[i][j] = 1;
-            }
-            if (i > CirclesXCount / 2)
-            {
-                SecondAtom.setPosition(localX * i + DrawOffset, localY * j + DrawOffset);
-                win.draw(SecondAtom);
-                win.display();
-                CirclesArray[i][j] = 2;
-            }
-            if (i == CirclesXCount / 2)
-            {
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    // if (key == sf::Keyboard::Escape) mWindow.close();
+}
 
-                Vacancy.setPosition(localX * i + DrawOffset, localY * j + DrawOffset);
-                win.draw(Vacancy);
-                win.display();
-                CirclesArray[i][j] = 3;
-                VacancyArray[j].first = i;
-                VacancyArray[j].second = j;
+bool Game::Restart()
+{
+    return false;
+}
+
+void Game::BeginPlay() {}
+
+void Game::update(sf::Time deltaTime)
+{
+    atomGrid.MoveVacancy(deltaTime);
+}
+
+void Game::render(sf::Time deltaTime)
+{
+
+    mWindow.clear();
+    atomGrid.DisplayGrid(mWindow);
+
+    Atom1Chart.DrawSeries(atomGrid.ValsX, atomGrid.ValsY1, mWindow, Atom1Chart);
+    Atom2Chart.DrawSeries(atomGrid.ValsX, atomGrid.ValsY2, mWindow, Atom2Chart);
+
+    mWindow.display();
+}
+
+AtomGrid::AtomGrid()
+{
+    GridVector.resize(AtomOnSide);
+    int VacancyCounter = 0;
+    AtomRadius = SideSize / AtomOnSide / 2;
+    for (int x = 0; x < AtomOnSide; x++)
+    {
+        GridVector[x].resize(AtomOnSide);
+        for (int y = 0; y < AtomOnSide; y++)
+        {
+            GridVector[x][y].setRadius(AtomRadius);
+            GridVector[x][y].setPosition(Location.x + x * AtomRadius * 2, Location.y + y * AtomRadius * 2);
+
+            if (x < AtomOnSide / 2)
+            {
+                GridVector[x][y].setFillColor(clFirstAtom);
+            }
+            if (x >= AtomOnSide / 2)
+            {
+                GridVector[x][y].setFillColor(clSecondAtom);
+            }
+
+            float r = (rand() % 100);
+            if (r <= 100 * VacancyAmount / (AtomOnSide * AtomOnSide))
+            {
+                if (VacancyCounter < VacancyAmount)
+                {
+                    GridVector[x][y].setFillColor(clVacancy);
+                    VacancyCounter++;
+                    VacancyCoords.push_back(sf::Vector2i(x, y));
+                }
             }
         }
-    bGridDrawed = true;
-
-    return;
-}
-
-void Model::setting() {}
-void Model::ClearGrid(RenderWindow& win)
-{
-    win.clear();
-    bGridDrawed = false;
-}
-
-void Model::DrawCircleColor(RenderWindow& win, int x, int y, int num)
-{
-    float xLocal = (DrawOffset) + ((x)*2 * CircleRadius);
-    float yLocal = (DrawOffset) + ((y)*2 * CircleRadius);
-
-    if (num == 1)
-    {
-        win.draw(FirstAtom);
-        FirstAtom.setPosition(xLocal, yLocal);
     }
-    if (num == 2)
-    {
-        win.draw(SecondAtom);
-        SecondAtom.setPosition(xLocal, yLocal);
-    }
-    if (num == 3)
-    {
-        win.draw(Vacancy);
-        Vacancy.setPosition(xLocal, yLocal);
-    }
-
-    win.display();
+    CalculateAtom();
 }
 
-bool Math::RandBool()
+void AtomGrid::DisplayGrid(sf::RenderWindow& window)
 {
-    float r = (rand() % 100);
-    if (r >= 50)
-        return true;
-    else
-        return false;
+    for (int x = 0; x < AtomOnSide; x++)
+    {
+        for (int y = 0; y < AtomOnSide; y++)
+        {
+            window.draw(GridVector[x][y]);
+        }
+    }
 }
 
-void Model::CalculateAtoms()
+void AtomGrid::MoveVacancy(sf::Time DeltaTime)
+{
+    ChartTimer += DeltaTime;
+
+    for (int i = 0; i < VacancyAmount; i++)
+    {
+        int xDirection = 0;
+        int yDirection;
+
+        float r = (rand() % 100);
+        if (r >= 50)
+        {
+            xDirection = 1;
+        }
+        else
+        {
+            xDirection = -1;
+        }
+
+        r = (rand() % 100);
+        if (r >= 50)
+        {
+            yDirection = 1;
+        }
+        else
+        {
+            yDirection = -1;
+        }
+
+        if (VacancyCoords[i].x + xDirection >= AtomOnSide) xDirection *= -1;
+        if (VacancyCoords[i].x + xDirection < 0) xDirection *= -1;
+
+        if (VacancyCoords[i].y + yDirection >= AtomOnSide) yDirection *= -1;
+        if (VacancyCoords[i].y + yDirection < 0) yDirection *= -1;
+
+        sf::Color clLastAtom = GridVector[VacancyCoords[i].x + xDirection][VacancyCoords[i].y + yDirection].getFillColor();
+
+        if (clLastAtom == clVacancy)
+        {
+            yDirection = 0;
+            xDirection = 0;
+            clLastAtom = GridVector[VacancyCoords[i].x + xDirection][VacancyCoords[i].y + yDirection].getFillColor();
+        }
+
+        GridVector[VacancyCoords[i].x][VacancyCoords[i].y].setFillColor(clLastAtom);
+
+        GridVector[VacancyCoords[i].x + xDirection][VacancyCoords[i].y + yDirection].setFillColor(clVacancy);
+
+        VacancyCoords[i] = sf::Vector2i(VacancyCoords[i].x + xDirection, VacancyCoords[i].y + yDirection);
+    }
+    if (ChartTimer.asSeconds() > 0.5f)
+    {
+        ChartTimer = sf::seconds(0);
+        CalculateAtom();
+    }
+}
+
+void AtomGrid::CalculateAtom()
 {
     ValsX.clear();
     ValsY1.clear();
     ValsY2.clear();
 
-    for (int x = 0; x < CirclesXCount; x++)
+    for (int x = 0; x < AtomOnSide; x++)
     {
         ValsX.push_back(x);
         int count1Atom = 0;
         int count2Atom = 0;
 
-        for (int y = 0; y < CirclesYCount; y++)
+        for (int y = 0; y < AtomOnSide; y++)
         {
-            if (CirclesArray[x][y] == 1) count1Atom++;
-            if (CirclesArray[x][y] == 2) count2Atom++;
+            if (GridVector[x][y].getFillColor() == clFirstAtom) count1Atom++;
+            if (GridVector[x][y].getFillColor() == clSecondAtom) count2Atom++;
         };
 
         ValsY1.push_back(count1Atom);
@@ -248,65 +235,51 @@ void Model::CalculateAtoms()
     }
 }
 
-void Model::CalculateSize()
-{
-    GraficLoc.second = CirclesYCount * CircleRadius * 2 + DrawOffset * 2 + CirclesYCount * CircleRadius;
-    GraficLoc.first = DrawOffset;
-
-    GraficSize.first = CirclesXCount * CircleRadius * 2;
-    GraficSize.second = CirclesYCount * CircleRadius;
-}
-
-
-LineChart::LineChart(int pPoints, pair<int, int> pLocation, pair<int, int> pSize, Color pColor)
+LineChart::LineChart(int pPoints, sf::Vector2f location, sf::Vector2f size, sf::Color pColor)
 {
     Points = pPoints;
-    Location = pLocation;
-    Size = pSize;
+    Location = location;
+    Size = size;
     color = pColor;
 
-    VertexArray::setPrimitiveType(PrimitiveType::LinesStrip);
+    VertexArray::setPrimitiveType(sf::PrimitiveType::LinesStrip);
     VertexArray::resize(pPoints);
 }
 
-void LineChart::DrawSeries(vector<float>& pValsX, vector<float>& pValsY, RenderWindow& win, LineChart& instance)
+void LineChart::DrawSeries(vector<float>& pValsX, vector<float>& pValsY, sf::RenderWindow& win, LineChart& instance)
 {
-    Vector2f vector;
+    sf::Vector2f vector;
 
     for (int x = 0; x < Points; x++)
     {
-        float a = Size.first / Points * pValsX[x] + Location.first;
-        float b = Location.second - (pValsY[x] * Size.second / Points);
+        float a = Size.x / (Points - 1) * pValsX[x] + Location.x;
+        float b = Location.y - (pValsY[x] / Points * Size.y);
+        vector = sf::Vector2f(a, b);
 
-        vector = Vector2f(a, b);
         instance[x].position = vector;
         instance[x].color = color;
-        /*
-                cout << "x: " << x << endl;
-        cout << "a: " << a << endl;
-        cout << "b: " << b << endl;
-        cout << "Points: " << Points << endl;
-        cout << "Size.first: " << Size.first << endl;
-        cout << "Size.second: " << Size.second << endl;
-        cout << "Location.first: " << Location.first << endl;
-        cout << "Location.second: " << Location.second << endl;
-        */
+        sf::CircleShape shape;
+
+        shape.setRadius(3.f);
+        shape.setPosition(vector.x - 1.5f, vector.y - 1.5f);
+        shape.setFillColor(color);
+        win.draw(shape);
     };
 
-    win.draw(instance);
+    win.draw(*this);
 }
 
-void LineChart::DrawBackground(Color color, float Offset, RenderWindow& win)
+void LineChart::DrawBackground(sf::Color color, float Offset, sf::RenderWindow& win)
 {
-    RectangleShape GraficField;
-    float x = Location.first - Offset / 2;
-    float y = Location.second - Size.second - Offset / 2;
+    sf::RectangleShape GraficField;
+    float x = Location.x - Offset / 2;
+    float y = Location.y - Size.y - Offset / 2;
 
-    Vector2f vector = Vector2f(x, y);
+    sf::Vector2f vector = sf::Vector2f(x, y);
 
     GraficField.setFillColor(color);
     GraficField.setPosition(vector);
-    Vector2f GraficSize(Size.first + Offset / 2, Size.second + Offset);
+    sf::Vector2f GraficSize(Size.x + Offset / 2, Size.y + Offset);
     GraficField.setSize(GraficSize);
 
     win.draw(GraficField);
--- sfmlLab/Main.h
+++ sfmlLab/Main.h
@@ -1,70 +1,80 @@
-#pragma once
-
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 #include "SFML/Graphics.hpp"
 #include "iostream"
 
-using namespace sf;
 using namespace std;
 
-const class Model
+class LineChart : sf::VertexArray
 {
 public:
-    const int CirclesXCount = 35;
-    const int CirclesYCount = 35;
-    float timer = 0;
-    float timerG = 0;
+    LineChart(int pPoints, sf::Vector2f location, sf::Vector2f size, sf::Color pColor = sf::Color::Red);
+    void DrawSeries(vector<float>& pValsX, vector<float>& pValsY, sf::RenderWindow& win, LineChart& instance);
+    void DrawBackground(sf::Color color, float Offset, sf::RenderWindow& win);
 
-    float time1 = 30.0f;
-    float time2 = 300.0f;
+private:
+    int Points = 0;
+    sf::Vector2f Location;
+    sf::Vector2f Size;
+    sf::Color color = sf::Color::Red;
+};
 
-    int CirclesArray[35][35];
+class AtomGrid
+{
+public:
+    AtomGrid();
+    void DisplayGrid(sf::RenderWindow& window);
+    void MoveVacancy(sf::Time DeltaTime);
+    int GetAtomOnSide() { return AtomOnSide; };
+
+    int SideSize = 500;
+    sf::Vector2f Location = sf::Vector2f(20.f, 20.f);
 
     vector<float> ValsX;
     vector<float> ValsY1;
     vector<float> ValsY2;
 
+    sf::Color clFirstAtom = sf::Color::Red;
+    sf::Color clSecondAtom = sf::Color::Green;
+    int AtomRadius = 0;
 
-    pair<int, int> VacancyArray[35];
-    CircleShape FirstAtom;
-    CircleShape SecondAtom;
-    CircleShape Vacancy;
-    CircleShape Black;
+private:
+    void CalculateAtom();
 
-    Color clFirstAtom = Color(255, 0, 0);
-    Color clSecondAtom = Color(0, 255, 0);
-    Color clVacancy = Color(255, 255, 255);
+private:
+    int AtomOnSide = 25;
+    int VacancyAmount = 40;
 
-    float CircleRadius = 6.0f;
-    float DrawOffset = 35.0f;
+    vector<vector<sf::CircleShape>> GridVector;
+    vector<sf::Vector2i> VacancyCoords;
 
-    int xCoords = 1;
-    int yCoords = 1;
-
-    bool bGridDrawed = false;
-
-    void SetCircleColor();
-    void DrawCircles(RenderWindow& win, float time);
-    void DrawCircleGrid(RenderWindow& win);
-    void setting();
-    void ClearGrid(RenderWindow& win);
-    void DrawCircleColor(RenderWindow& win, int x, int y, int num);
-
-    void CalculateAtoms();
-    void CalculateSize();
-
-    pair<int, int> GraficSize;
-    pair<int, int> GraficLoc;
-
-    RectangleShape GraficField;
-    // void SetVars();
-    //  void DrawGrafic(RenderWindow& win,float time);
+    sf::Color clVacancy = sf::Color::White;
+    sf::Time ChartTimer = sf::seconds(0);
 };
 
-const class Math
+class Game
 {
 public:
-    bool RandBool();
-};
+    Game();
+    bool run();
 
+private:
+    void BeginPlay();
+    void processEvents();
+    void update(sf::Time deltaTime);
+    void render(sf::Time deltaTime);
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+    bool Restart();
+
+private:
+    AtomGrid atomGrid;
+    LineChart Atom1Chart;
+    LineChart Atom2Chart;
+
+    sf::ContextSettings settings;
+    sf::RenderWindow mWindow;
+    sf::CircleShape shape;
+
+    int TargetFPS = 45;
+    sf::Time TimePerFrame = sf::seconds(1.f / TargetFPS);
+};
